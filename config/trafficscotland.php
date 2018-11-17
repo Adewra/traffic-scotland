fc300a736b940b4d7fbbdb9ecf5660d70c8e942e
Ally Dewar <Alastair@adewra.com>
2018-11-17 13:49:26 +0000

    Added template config

---
+++ config/trafficscotland.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * User: Ally Dewar
+ * Date: 14/11/2018
+ * Time: 22:13
+ */
+return [
+    // ...
+    'trafficscotland' => [
+        'method' => env('TRAFFICSCOTLAND_METHOD','rss'),
+    ]
+];
