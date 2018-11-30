27112739630fecd084d1f23ce96765e63c463d4b
Ally Dewar <Alastair@adewra.com>
2018-11-30 22:34:59 +0000

    Added a Region Enum to Helpers

--- src/Helpers/Regions.php
+++ src/Helpers/Regions.php
@@ -1,15 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: allydewar
- * Date: 30/11/2018
- * Time: 21:39
- */
 
 namespace Adewra\TrafficScotland\Helpers;
 
-
-class Regions
+class Regions extends \SplEnum
 {
+    const __default = self::Strathclyde;
 
+    const HighlandAndWesternIsles = 1;
+    const Grampian = 2;
+    const CentralTaysideAndFife = 3;
+    const Strathclyde = 4;
+    const SouthWestScotlandLothianAndBorders = 5;
 }
