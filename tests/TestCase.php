610601a5c0df883ce9e74ab933f543ed339a2ac5
Ally Dewar <Alastair@adewra.com>
2018-12-01 11:48:57 +0000

    Updates to testing

---
+++ tests/TestCase.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Adewra\TrafficScotland\Test;
+use Adewra\TrafficScotland\TrafficScotlandFacade;
+use Adewra\TrafficScotland\TrafficScotlandServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+class TestCase extends OrchestraTestCase
+{
+    /**
+     * Setup the test environment.
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Load package service provider
+     * @param  \Illuminate\Foundation\Application $app
+     * @return Illuminate\Support\ServiceProvider[]
+     */
+    protected function getPackageProviders($app)
+    {
+        return [TrafficScotlandServiceProvider::class];
+    }
+    /**
+     * Load package alias
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            'TrafficScotland' => TrafficScotlandFacade::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'sqlite');
+    }
+}
