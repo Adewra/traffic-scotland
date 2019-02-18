6183dcaab51b69168b924500a338fc29625b22a2
Ally Dewar <Alastair@adewra.com>
2019-02-18 21:05:48 +0000

    Added a question to an empty command

--- src/Console/RoadworksCommand.php
+++ src/Console/RoadworksCommand.php
@@ -33,6 +33,25 @@ class RoadworksCommand extends Command
      */
     public function handle(RoadworksController $controller)
     {
+        if($this->option('current') == null && $this->option('planned') == null)
+            $currentPlannedOrBoth = $this->anticipate('Would you like current, planned or both?', ['current','planned','both']);
+
+        if(isset($currentPlannedOrBoth))
+        {
+            switch (strtolower($currentPlannedOrBoth)) {
+                case "current":
+                    $this->option('current', true);
+                    break;
+                case "planned":
+                    $this->option('planned', true);
+                    break;
+                case "both":
+                    $this->option('current', true);
+                    $this->option('planned', true);
+                    break;
+            }
+        }
+
         $controller->index($this->option('current') ?? false, $this->option('planned') ?? false);
         return;
     }
